d205750278b83e7a3a55de092e23e453ffafc9e3
lucapu88 <caputoluca88@gmail.com>
2019-12-10 17:27:45 +0100

    fine jsnack3

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -1,2 +1,11 @@
 // jsnack4: Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+var array1 = [1, 2, 3, 4, 5];
+var array2 = ['a', 'b', 'c', 'd', 'e'];
+
+function fusion(a,b) {
+  for (var i = 0; i < 5; i++) {
+    
+  }
+}
